159e6cfbfd635d857b4d10f5603672045c0ca2e1
Hasan Islam <kinghasanbd1@gmail.com>
2025-11-15 20:35:41 +0600

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,7 @@ import './globals.css'
 
 const spaceGrotesk = Space_Grotesk({ 
   subsets: ['latin'],
+  weight: ['300', '400', '500', '600', '700'],
   variable: '--font-space-grotesk'
 })
 
@@ -14,8 +15,8 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={`${spaceGrotesk.variable} font-sans`}>
+    <html lang="en" className={spaceGrotesk.variable}>
+      <body className="font-sans">
         {children}
       </body>
     </html>
