6eaa573749dd22b8485a4678a9d3a3c526889bd2
Hasan Islam <kinghasanbd1@gmail.com>
2025-11-15 20:42:42 +0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,12 @@ module.exports = {
   content: [
     './app/**/*.{js,ts,jsx,tsx,mdx}',
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       fontFamily: {
-        'space-grotesk': ['Space Grotesk', 'sans-serif'],
+        'sans': ['var(--font-space-grotesk)', 'system-ui', 'sans-serif'],
       },
     },
   },
